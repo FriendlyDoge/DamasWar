38f40526570860d9896c820939925426f8d2c7cb
Luciano Plouvier <luciano.plouvier@gmail.com>
2020-11-21 19:54:03 -0300

    Testes da classe BoardPreparator

---
+++ DamasBattle/Assets/EditMode  Tests/TestBoardPreparator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestBoardPreparator
+    {
+        
+        [Test]
+        public void TestIsDarkSquare()
+        {
+            BoardPreparator preparator = new BoardPreparator();
+            int i = 0;
+            int j = 0;
+            bool result = preparator.IsDarkSquare(0,0);
+            Assert.AreEqual(true, result);
+
+            result = preparator.IsDarkSquare(0, 1);
+            Assert.AreEqual(false, result);
+            result = preparator.IsDarkSquare(1, 0);
+            Assert.AreEqual(false, result);
+            result = preparator.IsDarkSquare(1, 1);
+            Assert.AreEqual(true, result);
+
+
+        }
+
+        [Test]
+        public void TestPreparePieces()
+        {
+
+        }
+
+    }
+}
---
+++ DamasBattle/Assets/PlayMode Tests/TestBoardPreparator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestBoardPreparator
+    {
+
+        [SerializeField] BoardPreparator preparatorPrefab = null;
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestPrepareBoard()
+        {
+            GameObject obj = new GameObject();
+            BoardPreparator prep = obj.AddComponent<BoardPreparator>();
+            GameObject pieceObj = new GameObject();
+            pieceObj.AddComponent<Piece>();
+
+            prep.setTestData(pieceObj.GetComponent<Piece>());
+
+            Piece[][] pieces = prep.PrepareBoard();
+            Assert.AreEqual(10, pieces.Length);
+            for(int i = 0; i < pieces.Length; i++)
+            {
+                Assert.AreEqual(10, pieces[i].Length);
+            }
+
+        }
+    }
+}
